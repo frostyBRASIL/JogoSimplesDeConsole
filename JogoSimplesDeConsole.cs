37243a7837cf4a68d2ef248c59593e6daa62fc5f
Lucas Moura <61442944+frostyBRASIL@users.noreply.github.com>
2023-09-03 00:43:14 -0300

    Adicionando classe base de herança
    Adição de uma classe base com o nome de atributos, que serve de herança para as classes Jogador e Inimigo.

--- JogoSimplesDeConsole.cs
+++ JogoSimplesDeConsole.cs
@@ -1,39 +1,41 @@
-﻿using System;
+using System;
 
 namespace JogoSimplesDeConsole
 {
-    public class Jogador
-        {
-            public string Nome;
-            public bool Vivo;
-            public int Vida;
-            public bool Atacar;
-            public bool EscolhaAtaque;
-            public Random ForcaAtaque;
-            public bool EscolhaAtaqueEsp;
-            public Random ForcaAtaqueEsp;
-            public int QuantBrasoes;
-            protected bool TemBrasoes;
+    public class Atributos
+    {
+        public string nome;
+        public bool vivo;
+        public int vida;
+        public int ataque;
+        public bool atacar;
+        public Random ForcaAtaque;
+        
+    }
 
-            public Jogador()
-            {
-                Vivo = true;
-                Vida = 150;
-                Atacar = true;
-                EscolhaAtaque = true;
-                ForcaAtaque = new Random();
-                EscolhaAtaqueEsp = false;
-                ForcaAtaqueEsp = new Random();
-                QuantBrasoes = 19;
-                TemBrasoes = true;
-            }
-        }
-        public class Boss
+    public class Jogador : Atributos
+    {
+        public bool escolhaAtaque;
+        public bool EscolhaAtaqueEsp;
+        public Random ForcaAtaqueEsp;
+        public int QuantBrasoes;
+        protected bool TemBrasoes;
+
+        public Jogador()
         {
-            public string Nome;
-            public double Vida;
-            public bool Vivo;
-            public bool Atacar;
+            vivo = true;
+            vida = 150;
+            Random ataque = new Random();
+            escolhaAtaque = true;
+            ForcaAtaque = new Random();
+            EscolhaAtaqueEsp = false;
+            ForcaAtaqueEsp = new Random();
+            QuantBrasoes = 19;
+            TemBrasoes = true;
+        }
+    }
+        public class Boss : Atributos
+        {
             public Random ForcaAtaque;
             public int Fases;
             public bool PriFase;
@@ -41,22 +43,22 @@ namespace JogoSimplesDeConsole
 
             public Boss()
             {
-                Nome = "Inimigo";
-                Vida = 300;
-                Vivo = true;
-                Atacar = true;
+                nome = "Joãozinho";
+                vida = 300;
+                vivo = true;
+                atacar = true;
                 Random ForcaAtaque = new Random();
                 Fases = 2;
                 PriFase = true;
             }
         }
-    
+
     class Principal
     {
         static void SistemaAtaqueJ(Jogador j1, Boss boss) // Função que define o sistema de ataque do jogador
         {
             Console.Clear();
-            j1.EscolhaAtaque = true;
+            j1.escolhaAtaque = true;
             j1.EscolhaAtaqueEsp = false;
             j1.QuantBrasoes -= 2;
     
@@ -65,13 +67,13 @@ namespace JogoSimplesDeConsole
             object atq = j1.ForcaAtaque.Next(10, 50);
     
             Console.WriteLine($"Ótimo, você causou {atq} de dano");
-            Console.WriteLine($"{boss.Nome} teve a vida reduzida para: {boss.Vida - Convert.ToDouble(atq)}");
+            Console.WriteLine($"{boss.nome} teve a vida reduzida para: {boss.vida - Convert.ToDouble(atq)}");
             Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
         }
         static void SistemaAtaqueEsp(Jogador j1, Boss boss) // Função que define o sistema de ataque especial do jogador
         {
             Console.Clear();
-            j1.EscolhaAtaque = false;
+            j1.escolhaAtaque = false;
             j1.EscolhaAtaqueEsp = true;
             j1.QuantBrasoes -= 4;
     
@@ -81,16 +83,16 @@ namespace JogoSimplesDeConsole
     
             Console.WriteLine($"Ótimo, você causou {atq} de dano");
             Console.WriteLine($"Seus brasões espírituais foram para: {j1.QuantBrasoes}");
-            Console.WriteLine($"{boss.Nome} teve a vida reduzida para: {boss.Vida - Convert.ToDouble(atq)}");
+            Console.WriteLine($"{boss.nome} teve a vida reduzida para: {boss.vida - Convert.ToDouble(atq)}");
             Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
         }
         static void SistemaDefesa(Jogador j1, Boss boss) // Função qye defibe o sistema de defesa do jogador
         {
             Console.Clear();
             object atqBoss = j1.ForcaAtaque.Next(40);
-            j1.Vida = j1.Vida - Convert.ToInt32(atqBoss);
+            j1.vida = j1.vida - Convert.ToInt32(atqBoss);
     
-            Console.WriteLine($"Você defendeu e sofreu menos do dano total. Você ainda tem {j1.Vida} de vida.");
+            Console.WriteLine($"Você defendeu e sofreu menos do dano total. Você ainda tem {j1.vida} de vida.");
             Console.WriteLine($"Dano recebido: {atqBoss}");
             Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
     
@@ -107,40 +109,40 @@ namespace JogoSimplesDeConsole
             Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
     
             Console.Write("Escolha seu nome: ");
-            j1.Nome = Console.ReadLine();
+            j1.nome = Console.ReadLine();
     
             inicio:
     
             Console.Clear();
             
             Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-            Console.WriteLine($"|\tOs atributos do {b1.Nome} são:\t\t|");
-            Console.WriteLine($"|\tVida................: {j1.Vida}\t\t|");
+            Console.WriteLine($"|\tOs atributos do {b1.nome} são:\t\t|");
+            Console.WriteLine($"|\tVida................: {j1.vida}\t\t|");
             Console.WriteLine($"|\tForça do ataque.....: 40\t\t| ");
             Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
             
             Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
             Console.WriteLine($"|\tSeus atributos são:\t\t\t|");
-            Console.WriteLine($"|\tVida......................: {b1.Vida}\t\t|");
+            Console.WriteLine($"|\tVida......................: {b1.vida}\t\t|");
             Console.WriteLine($"|\tQuantidade de brasões.....: {j1.QuantBrasoes}\t\t|");
             Console.WriteLine($"|\tAtaque....................: 10 - 50\t|");
             Console.WriteLine($"|\tAtaque Especial...........: 60 - 150\t|");
             Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
             
-            Console.WriteLine($"{j1.Nome}, a batalha começou, deseja atacar {b1.Nome}?");
+            Console.WriteLine($"{j1.nome}, a batalha começou, deseja atacar {b1.nome}?");
             Console.WriteLine("(S/N)");
             string atacar = Console.ReadLine();
     
             //Definindo as possiveis opções
             if (atacar.StartsWith("s")  || atacar.StartsWith("S"))
             {
-                j1.EscolhaAtaque = j1.EscolhaAtaque;
+                j1.escolhaAtaque = j1.escolhaAtaque;
     
                 comecoataque:
     
                 Console.Clear();
                 Console.WriteLine($"\nVocê possui {j1.QuantBrasoes} brasões");
-                Console.WriteLine($"{j1.Nome}, qual ataque você gostaria de usar?");
+                Console.WriteLine($"{j1.nome}, qual ataque você gostaria de usar?");
                 Console.WriteLine("(1) Ataque (Custo de brasões: 2) | (2) Ataque Especial (Custo de brasões: 4)");
                 string tipoAtaque = Console.ReadLine();
     
@@ -160,7 +162,7 @@ namespace JogoSimplesDeConsole
             }
             else if (atacar.StartsWith("n") || atacar.StartsWith("N"))
             {
-                j1.Atacar = false;
+                j1.atacar = false;
                 SistemaDefesa(j1, b1);
             }
             else
